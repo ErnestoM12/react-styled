1e9337f9bef5a240ac8edbb1e4cc01f69dd881d0
Ernesto Maya <emaya167@gmail.com>
2022-01-16 21:12:20 -0600

    primer ejemplo unsado styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components';
 
+const Header = styled.header`
+background:linear-gradient(20deg, #db7093, #daa357);
+text-align:center;
+border-radius:0.2em;
+color:#fff;
+padding:0.3em;
+margin:0.3em;
+font-size:14px;
+`
 
 const App = () => {
   return (
     <div>
+      <Header>
+        <h1>Hola</h1>
+      </Header>
     </div>
   );
 }
