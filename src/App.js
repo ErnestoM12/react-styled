a48c895f206fea75464ce5a96bd365a87e528bb0
Ernesto Maya <emaya167@gmail.com>
2022-01-17 01:00:28 -0600

    propiedad css styled

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { mainColor, secondaryColor, paddign, getLinearGradient } from './styles'
 
-
-const sizes = {
-  mobile: '375px',
-  tablet: '768px',
-  desktop: '1024px'
-}
-
-const device = {
-  mobile: (styles) => {
-    return `@media(max-width:${sizes.mobile}){
-      ${styles}
-    }`
-  },
-  tablet: (styles) => {
-    return `@media(max-width:${sizes.tablet}){
-      ${styles}
-    }`
-  },
-  desktop: (styles) => {
-    return `@media(max-width:${sizes.desktop}){
-      ${styles}
-    }`
-  },
-}
 
 
 const Header = styled.header`
@@ -36,23 +12,17 @@ color:#fff;
 ${paddign}
 margin:0.3em;
 font-size:20px;
+`
 
+const baseStyles = css`
+padding: 1em 2em;
+margin: 1em;
+border: ${props => `2px solid ${props.borderColor}`};
+`
 
-${device.mobile`
- ${getLinearGradient('20deg', secondaryColor, 'green')}
-  font-size:12px;
-  h1{
-    color:red
-  }
-`}
+const Button = styled.button`
+${baseStyles}`
 
-${device.tablet`
- ${getLinearGradient('20deg', secondaryColor, 'red')} 
-  font-size:18px;
-  h1{
-    color:black
-  }
-`}`
 
 
 
@@ -64,6 +34,12 @@ const App = () => {
           <h1>Hola</h1>
         </div>
       </Header>
+      <Button>
+        Dispara
+      </Button>
+      <Button borderColor='green'>
+        otro boton
+      </Button>
     </div>
   );
 }
