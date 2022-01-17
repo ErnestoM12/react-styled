c08b2cec9dd23deb6cc49354270385e339f43294
Ernesto Maya <emaya167@gmail.com>
2022-01-16 22:41:07 -0600

    compononentes dinamicos con props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { mainColor, secondaryColor, paddign, getLinearGradient } from './styles'
 
@@ -12,55 +12,44 @@ border-radius:0.2em;
 color:#fff;
 ${paddign}
 margin:0.3em;
-font-size:14px;
-transition: all 350ms ease-in-out;
-opacity: 0.6;
-/**
-*agregando pseudo clases 
-*/
-&:hover {
-opacity: 1;
-  
-h1{
-  color:black
-}
-
-}
-
-div{
-  background:red
+font-size:14px;`
 
 
-}
-h1{
-color: white;
-}
+const Button = styled.button`
+padding: 0.6em 1.5em;
+${(props) => 'background:' + (props.bg || 'black')}; 
+border-radius: 0.1em;
+color:#fff;
+border:0;
+margin: 0.4em;`
 
-.big {
-  font-size: 50px;
-}
-
-`
-
-const SubTitle = styled.h2`
-color:${secondaryColor};`
 
 
 
 const App = () => {
+
+  const [Clicks, setClicks] = useState(0)
+
+  const aumentar = () => setClicks(Clicks + 1)
+
+
   return (
     <div>
       <Header>
         <div>
           <h1>Hola</h1>
         </div>
-        <span className='big'>
-          Ejemplo
-        </span>
       </Header>
-      <SubTitle>
-        Ejemplo de subtitulo
-      </SubTitle>
+      <Button
+        bg={secondaryColor}
+        onClick={aumentar}
+      >
+        un boton
+      </Button>
+      <Button>
+        un boton
+      </Button>
+      {Clicks}
     </div>
   );
 }
